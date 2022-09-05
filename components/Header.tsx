6ff6ee53f6383b5e216e2a780f8679300a5e5878
Chaeri-Yoon <cryoon0103@gmail.com>
2022-09-05 23:52:11 +0900

    * Header   - When a button on the nav bar is clicked, the scroll will directly move to the correnspondent section.

---
+++ components/Header.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPizzaSlice } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+
+const Container = styled.div`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 2em;
+    padding: 1.5em 3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #ff9d00;
+    color: white;
+
+    & > *{
+        width: 33%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        &:first-child{
+            justify-content: flex-start;
+        }
+        &:last-child{
+            justify-content: flex-end;
+        }
+    }
+`;
+const Logo = styled.div`
+    & > *{
+        width: 1.5em;
+        aspect-ratio: 1 / 1;
+    }
+`;
+const NavContainer = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 600;
+    & > *{
+        padding: 0 10%;
+        &:not(:last-child){
+            border-right: 2px solid white;
+        }
+    }
+`;
+const UserContainer = styled(NavContainer)`
+    & > *{
+        padding: 0 5%;
+        &:not(:last-child){
+            border-right: 2px solid white;
+        }
+    }
+`;
+export default () => {
+    return (
+        <Container>
+            <Logo>
+                <FontAwesomeIcon icon={faPizzaSlice} />
+            </Logo>
+            <NavContainer>
+                <Link href="#home">Home</Link>
+                <Link href="#menu">Menu</Link>
+                <Link href="#contact">Contact</Link>
+            </NavContainer>
+            <UserContainer>
+                <span>Join</span>
+                <span>Login</span>
+            </UserContainer>
+        </Container>
+    )
+}
