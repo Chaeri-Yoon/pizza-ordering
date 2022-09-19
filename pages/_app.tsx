b5990acff02e00744495c19b0e5f95b955ae4874
Chaeri-Yoon <cryoon0103@gmail.com>
2022-09-19 23:11:43 +0900

    Change the way of connecting the app to db to the one using useSWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import type { AppProps } from 'next/app';
-import { SWRConfig } from 'swr';
+import useSWR, { SWRConfig } from 'swr';
 import Header from '../components/Header';
-import useApi from '../lib/useApi';
 import '../styles/globals.css';
 
+function DBConnect() {
+  useSWR('/api/db');
+  return null;
+}
 function MyApp({ Component, pageProps }: AppProps) {
-  const [dbConnect] = useApi('/api/db');
-  useEffect(() => {
-    dbConnect();
-  }, []);
   return (
     <SWRConfig value={{ fetcher: (url: string) => fetch(url).then(response => response.json()) }}>
       <Header />
       <Component {...pageProps} />
+      <DBConnect />
     </SWRConfig>
   )
 }
